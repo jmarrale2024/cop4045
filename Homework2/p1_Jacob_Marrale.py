0bde4ece512d334a224c1a14bd496744d4585209
Jacob Marrale <jmarrale2024@fau.edu>
2026-09-15 15:30:09 -0400

    Function done for 1a maybe

--- Homework2/p1_Jacob_Marrale.py
+++ Homework2/p1_Jacob_Marrale.py
@@ -2,10 +2,17 @@
 
 def line_number(read_file: str , write_file: str) -> str: # read first file , write its line prefixed with line numbers for second
     try:
+        with open(read_file, 'r') as file1:
+            lines = file1.readlines() # would use generator here but question didn't require it so just gonna load it all at once
+        with open(write_file, 'w') as file2:
+            for i, line in enumerate(lines, start=1):
+                file2.write(f"{i}: {line}")
+                print(f"{i}: {line}") # track if I did it right
     except Exception as e:
         print(f"Function Failure: {e}")
         raise
 def main():
+
 
 
 if __name__ == "__main__":
