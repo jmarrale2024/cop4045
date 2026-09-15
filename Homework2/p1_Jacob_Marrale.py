fc9f223075160fc11519425edd28d900a978e0f9
Jacob Marrale <jmarrale2024@fau.edu>
2026-09-15 14:58:32 -0400

    Base outline for question 1a based on requirements (exception block, etc)

---
+++ Homework2/p1_Jacob_Marrale.py
@@ -0,0 +1,12 @@
+# homework 2
+
+def line_number(read_file: str , write_file: str) -> str: # read first file , write its line prefixed with line numbers for second
+    try:
+    except Exception as e:
+        print(f"Function Failure: {e}")
+        raise
+def main():
+
+
+if __name__ == "__main__":
+    main()
